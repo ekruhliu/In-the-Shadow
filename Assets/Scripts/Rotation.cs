b4d1f2ea90f038a6e8f796567e4b018d28a0c9b6
ekruhliu <hs_evg_322@icloud.com>
2019-05-14 19:03:07 +0300

    1

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -15,9 +15,20 @@ public class Rotation : MonoBehaviour
             click = false;
         if (click)
             OnMouseDrag();
-        
-        if ((gameObject.transform.rotation.x >= -3f && gameObject.transform.rotation.x <= 3f) && (gameObject.transform.rotation.y >= -3f && gameObject.transform.rotation.y <= 3f) && (gameObject.transform.rotation.z >= -5f && gameObject.transform.rotation.z <= 5f))
+        //Debug.Log("X: " + gameObject.transform.rotation.x + " Y: " + gameObject.transform.rotation.y + " Z: " + gameObject.transform.rotation.z + " W: " + gameObject.transform.rotation.w);
+        if (((gameObject.transform.rotation.x >= 0.0f && gameObject.transform.rotation.x <= 0.2f)
+            && (gameObject.transform.rotation.y >= 0.9f && gameObject.transform.rotation.y <= 1.0f)
+            && (gameObject.transform.rotation.z >= -0.1f && gameObject.transform.rotation.z <= 0.0f)
+            && (gameObject.transform.rotation.w >= 0.2f && gameObject.transform.rotation.w <= 0.3f)))
             Debug.Log("Completed");
+        else if (((gameObject.transform.rotation.x >= -0.1f && gameObject.transform.rotation.x <= 0.0f)
+                && (gameObject.transform.rotation.y >= -0.3f && gameObject.transform.rotation.y <= -0.2f)
+                && (gameObject.transform.rotation.z >= -0.1f && gameObject.transform.rotation.z <= 0.0f)
+                && (gameObject.transform.rotation.w >= 0.9f && gameObject.transform.rotation.w <= 1.0f)))
+            Debug.Log("Completed");
+        //right = 0.0-0.2, 0.9-1.0, 0.0- -0.1, 0.2-0.3
+        //left = 0.0-0.1, 0.2-0.3, 0.0-0.1, -1.0- -0.9
+        //transform.rotation.
     }
 
     void OnMouseDrag()
